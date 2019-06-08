2041d105374d2c13f756a2908505352a0bb6ef42
Janice Lee <jlad2017@mymail.pomona.edu>
2019-06-08 16:52:53 -0400

    Added functions to generate sparse arrays and exponentially decaying arrays

--- linearized_bregman/LB_comp.py
+++ linearized_bregman/LB_comp.py
@@ -6,17 +6,44 @@ Compares classic Linearized Bregman to modified Linearized Bregman
 import numpy as np
 
 def rand_exp_decay(n, a, b):
-"""
-???
-params:
-    n (int): desired number of elements in the vector
-    a (float): ???
-    b (float): ???
-returns:
-    none
-"""
-    A = log(a)
-    B = log(b)
-    R1 = A + (B-A) * np.rand(n)
-    R = np.square(np.exp(R1))
+    """
+    Creates a random semi-sparse array with exponentially decreasing elements
+    params:
+        n (int): desired number of elements in the vector
+        a (float): ???
+        b (float): ???
+    returns:
+        R (numpy array): array with exponentially decreasing elements
+    """
+    A = np.log(a)
+    B = np.log(b)
+    R1 = A + (B-A) * np.random.rand(n)
+    print(R1)
+    R = np.exp(R1*2)
+    print(R)
     idx = np.random.permutation(n)
+    for i in idx[1:int(np.floor(n/2))]:
+        R[i] = -R[i]
+    return R
+
+def rand_sparse(n, frac):
+    """
+    Creates a random sparse array
+    """
+    R = np.zeros(n, dtype=int)
+    idx = np.random.permutation(n)
+    for i in idx[1:(n/frac)]:
+        R[i] = np.random.rand(n)
+    return R
+
+
+def lb_compare(m, n, num_samp, max_iter):
+    """
+    Compares classic LB to modified LB
+    params:
+        m (int):
+        n (int):
+        num_samp (int):
+        max_iter (int):
+    returns:
+    """
