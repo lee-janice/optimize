977b8b7412c3eb16b44b32992aa68214afd720d2
Janice Lee <jlad2017@mymail.pomona.edu>
2019-06-11 20:37:24 -0400

    Separated reusable functions

---
+++ generate_vectors.py
@@ -0,0 +1,61 @@
+"""
+Generates vectors/arrays to use in optimization algorithms 
+@authors: Jimmy Singh and Janice Lee
+@date: June 11th, 2019
+"""
+import numpy as np
+import numpy.random as random
+import numpy.linalg as la
+np.random.seed(0)
+
+def rand_exp_decay(n, a, b):
+    """
+    Creates a random semi-sparse array with exponentially decreasing elements
+    params:
+        n (int): desired number of elements in the array
+        a (float): ???
+        b (float): ???
+    returns:
+        R (numpy array): array with exponentially decreasing elements
+    """
+    A = np.log(a)
+    B = np.log(b)
+    R1 = A + (B-A) * random.rand(n)
+    R = np.exp(R1*2)
+    idx = random.permutation(n)
+    for i in idx[0:int(np.floor(n/2))]:
+        R[i] = -R[i]
+    return R
+    
+def rand_sparse(n, frac):
+    """
+    Creates a random sparse array
+    params:
+        n (int): desired number of elements in the array
+        frac (int): 1/frac gives the fraction of non-zero elements in the array
+    returns:
+        R (numpy array): sparse array
+    """
+    R = np.zeros(n, dtype=float)
+    idx = random.permutation(n)
+    for i in idx[0:(n/frac)]:
+        R[i] = random.rand(1)
+    return R
+    
+def add_awgn_noise(x, snr_dB):
+    """
+    Function to add AWGN to a given signal 
+    Original function authored by Mathuranathan Viswanathan for MatLab/Octave
+    """
+    l = x.size
+    snr = 10 ** (snr_dB/10)
+    e_sym = np.sum(np.square(x)) / l
+    n0 = e_sym / snr
+    if (np.isreal(x).all()):
+        noise_sigma = np.sqrt(n0)
+        n = noise_sigma * random.randn(1, l) 
+    else:
+        noise_sigma = np.sqrt(n0/2)
+        n = noise_sigma * (random.randn(1, l) + 1j*random.randn(1, l))
+    y = x + n
+    return y
---
+++ plot.py
@@ -0,0 +1,96 @@
+"""
+Plots the results of the methods and saves them as figures
+@authors: Jimmy Singh and Janice Lee
+@date: June 11th, 2019
+"""
+import matplotlib.pyplot as plt
+import os 
+
+def get_title(sparse, noise, type):
+    """
+    Generates figure/plot titles
+    params: 
+        sparse (bool): true if the soln is sparse 
+        noise (bool): true if the data contains noise 
+        type (str): the type of data to be plotted (residual, 1-norm,  model error)
+    returns:
+        the generated figure/plot title 
+    """
+    title = "ISTA ("
+    # get sparse title 
+    if (sparse): 
+        title += "sparse soln, "
+    else:
+        title += "exponentally decaying soln, "
+    # get noise title 
+    if (noise):
+        title += "w/noise) - "
+    else: 
+        title += "w/out noise) - "
+    title += type.upper()
+    return title 
+
+def get_filename(sparse, noise, type):
+    """
+    Generates figure/plot filenames 
+    params: 
+        sparse (bool): true if the soln is sparse 
+        noise (bool): true if the data contains noise 
+        type (str): the type of data to be plotted (residual, 1-norm,  model error)
+    returns:
+        the generated figure/plot filename 
+    """
+    if not os.path.exists("plots/"):
+        os.mkdir("plots/")
+        
+    filename = "plots/" + type
+    # get sparse filename 
+    if (sparse): 
+        filename += "_sparse_"
+    else:
+        filename += "_randExpDecay_"
+    # get noise filename 
+    if (noise):
+        filename += "noise.png"
+    else: 
+        filename += "noNoise.png"
+    return filename
+
+def plot_lb(max_iter, data, sparse, noise, type):
+    """
+    Plots the data and saves the plot
+    params:
+        max_iter (int): the number of iterations of the algorithm executed
+        data (array-like): the data values to be plotted 
+        sparse (bool): true if the soln is sparse 
+        noise (bool): true if the data contains noise 
+        type (str): the type of data to be plotted (residual, 1-norm,  model error)
+    returns: 
+        none 
+    """
+    plt.clf()
+    plt.plot(range(1, max_iter+1), data)
+    plt.legend(["Classic", "Modified", "Modified w/out threshold"])
+    plt.xlabel("Number of iterations")
+    plt.ylabel("Data values")
+    plt.title(get_title(sparse, noise, type))
+    plt.savefig(get_filename(sparse, noise, type))
+    
+def plot_ista(max_iter, data, sparse, noise, type):
+    """
+    Plots the data and saves the plot
+    params:
+        max_iter (int): the number of iterations of the algorithm executed
+        data (array-like): the data values to be plotted 
+        sparse (bool): true if the soln is sparse 
+        noise (bool): true if the data contains noise 
+        type (str): the type of data to be plotted (residual, 1-norm,  model error)
+    returns: 
+        none 
+    """
+    plt.clf()
+    plt.plot(range(1, max_iter+1), data)
+    plt.xlabel("Number of iterations")
+    plt.ylabel("Data values")
+    plt.title(get_title(sparse, noise, type))
+    plt.savefig(get_filename(sparse, noise, type))
